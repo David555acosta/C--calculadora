89b15b5d6bfab661fc9070eeee89d250d814477d
David facha <davidisrraelacostaz.9@gmail.com>
2024-01-08 18:14:22 -0300

    Primeros pasos

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+
+            // 0:Declarar variables y sus tipos de datos (Int , Float , Char , Bool)
+            int n1, n2;
+            //1:Ingreso de datos.
+            Console.WriteLine("Ingrese un numeor:");
+            Console.ReadLine();
+            //2:Realizar calculo.
+            //3:Emitir en pantalla resultado/mostrar informacion
+            
+        }
+    }
+}
